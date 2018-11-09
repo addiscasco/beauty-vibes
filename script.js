51f684db6e2af383ebfdfe0490b4ee448de1c625
Addis Casco <33012886+addiscasco@users.noreply.github.com>
2018-11-09 17:32:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    //toggler btn navbar
+    $(".navbar-toggler").click(function(){
+        $(".navbar-toggler").toggleClass("change");
+    })
+
+    //sticky navbar less padding
+
+    $(window).scroll(function(){
+        let position=$(this).scrollTop();
+        
+        if(position>=718){
+            $(".navbar").addClass("navbar-background");
+            $(".navbar").addClass("fixed-top");
+        }
+        else{
+            $(".navbar").removeClass("navbar-background");
+            $(".navbar").removeClass("fixed-top");
+        }
+    })
+
+    //smoothscroll
+
+    $(".nav-item a, .header-link, #back-to-top").click(function(link){
+        link.preventDefault();
+        let target=$(this).attr("href");
+        console.log(target);
+        $("html, body").animate({
+            scrollTop: $(target).offset().top-25 
+        }, 3000);
+    })
+
+
+
+//backto  top
+
+$(window).scroll(function(){
+    let position=$(this).scrollTop();
+    
+    if(position>=718){
+        $("#back-to-top").addClass("scrollTop");
+    }
+    else{
+        $("#back-to-top").removeClass("scrollTop");
+    }
+  
+})
+
+
+
+
+    //ripples
+    $("#header, .info").ripples({
+        dropRadius:25,
+        perturbance:0.6,
+    })
+
+    //magnific popup
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image', 
+
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+})
